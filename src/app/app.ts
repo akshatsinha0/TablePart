96666e02bd283540d8efbabd3f877898aa3a5eaf
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-09-02 18:39:08 +0530

    quick text

--- src/app/app.ts
+++ src/app/app.ts
@@ -41,8 +41,6 @@ export class App implements AfterViewInit {
   skipMap = new Set<string>();
   selectedCells = new Set<string>();
   generatedCode = '';
-  displayedCode = '';
-  isTyping = false;
   
   private isDragging = false;
   private startCell: CellPosition | null = null;
@@ -159,8 +157,6 @@ export class App implements AfterViewInit {
     this.selectedCells.clear();
     this.currentSelection = [];
     this.generatedCode = '';
-    this.displayedCode = '';
-    this.isTyping = false;
   }
 
   generateCode() {
@@ -215,43 +211,12 @@ export class App implements AfterViewInit {
 </html>`;
 
     this.generatedCode = fullHtml;
-    this.startTypewriterAnimation(fullHtml);
   }
 
   saveCode() {
     if (this.generatedCode) {
       this.downloadCode(this.generatedCode);
     }
-  }
-
-  private startTypewriterAnimation(content: string) {
-    this.displayedCode = '';
-    this.isTyping = true;
-    let index = 0;
-    const baseSpeed = 15;
-    
-    const typeNextChar = () => {
-      if (index < content.length) {
-        const char = content.charAt(index);
-        this.displayedCode += char;
-        index++;
-        
-        let speed = baseSpeed;
-        if (char === '\n') {
-          speed = baseSpeed * 2;
-        } else if (char === ' ') {
-          speed = baseSpeed * 0.5;
-        } else if ('<>=/"'.includes(char)) {
-          speed = baseSpeed * 0.8;
-        }
-        
-        setTimeout(typeNextChar, speed);
-      } else {
-        this.isTyping = false;
-      }
-    };
-
-    setTimeout(typeNextChar, 300);
   }
 
   private downloadCode(content: string) {
